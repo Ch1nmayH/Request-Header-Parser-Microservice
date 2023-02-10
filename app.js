14d489388aed6766be2f8d7c109d684ca80826c3
ch1nmayh <cod1.1backup@gmail.com>
2023-02-10 23:48:26 +0530

    Get user ip address, language, software using request headers

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.set("trust proxy", true);
+
+app.get("/", (req, res) => {
+  let ipaddress = req.ip;
+  let language = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
+});
+
+app.listen(8000);
